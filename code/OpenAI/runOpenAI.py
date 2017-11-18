e49522c1812663dbcbe12a1749094994798aa054
Alexandre El Assad <aelassad@stanford.edu>
2017-11-17 18:57:28 -0800

    added run open AI to get actions/states

---
+++ code/OpenAI/runOpenAI.py
@@ -0,0 +1,27 @@
+import gym
+env = gym.make('FlyingPendulum-v0')
+# env.reset()
+# for _ in range(1000):
+#     env.render()
+#     env.step(env.action_space.sample()) # take a random action
+
+env = gym.make('FlyingPendulum-v0')
+for i_episode in range(1):
+    observation = env.reset()
+    f=open("states%s.txt" %i_episode, "a+")
+    f.write('\n'+','.join([str(a) for a in observation]))
+    f.close()
+    for t in range(10000):
+        env.render()
+        # print(observation)
+        action = env.action_space.sample()
+        f=open("actions%s.txt" %i_episode, "a+")
+        f.write('\n'+','.join([str(a) for a in action]))
+        f.close()
+        observation, reward, done, info = env.step(action)
+        f=open("states%s.txt" %i_episode, "a+")
+        f.write('\n'+','.join([str(a) for a in observation]))
+        f.close()
+        if done:
+            print("Episode finished after {} timesteps".format(t+1))
+            break
